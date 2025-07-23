5d17e46b030e5cbaaaf58cc25e8df58201e1cf2e
Fabian <42739411+challo2018@users.noreply.github.com>
2025-07-23 08:52:28 +0200

    URL encoding wrong
    ANSI / UTF8 URL Encoding problem changed

--- webfrontend/htmlauth/defines.php
+++ webfrontend/htmlauth/defines.php
@@ -7,10 +7,10 @@ define ("INSTALLDATA", TMPPREFIX . "installdata.json");
 
 define ("client_secret", "2e21faa1-db2c-4d0b-a10f-575fd372bc8c-575fd372bc8c");
 define ("callback_uri", "http://localhost:4200/");
-define ("apiURL", "https://api.viessmann-climatesolutions.​com/iot/v1/equipment/");
-define ("apiURLv2", "https://api.viessmann-climatesolutions.​com/iot/v2/features/");
-define ("authorize_URL", "https://iam.viessmann-climatesolutions.​com/idp/v2/authorize");
-define ("token_url", "https://iam.viessmann-climatesolutions.​com/idp/v2/token");
+define ("apiURL", "https://api.viessmann-climatesolutions.com/iot/v1/equipment/");
+define ("apiURLv2", "https://api.viessmann-climatesolutions.com/iot/v2/features/");
+define ("authorize_URL", "https://iam.viessmann-climatesolutions.com/idp/v2/authorize");
+define ("token_url", "https://iam.viessmann-climatesolutions.com/idp/v2/token");
 
 // The Navigation Bar
 $navbar[1]['Name'] = "Settings";
